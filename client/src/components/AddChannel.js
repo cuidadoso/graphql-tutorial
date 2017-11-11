4de97663c3447f3d656646f1291f7d1bfdd2b200
cuidadoso <alexander_s@front.ru>
2017-11-11 17:20:25 +0300

    Add fast/optimistic UI

--- client/src/components/AddChannel.js
+++ client/src/components/AddChannel.js
@@ -1,43 +1,57 @@
 import React from 'react';
-import { gql, graphql } from 'react-apollo';
+import {gql, graphql} from 'react-apollo';
 
-import { channelsListQuery } from './ChannelsListWithData';
+import {channelsListQuery} from './ChannelsListWithData';
 
-const AddChannel = ({ mutate }) => {
-  const handleKeyUp = (evt) => {
-    if (evt.keyCode === 13) {
-      evt.persist();
-      mutate({ 
-        variables: { name: evt.target.value },
-        refetchQueries: [ { query: channelsListQuery }],
-      })
-      .then( res => {
-        evt.target.value = '';  
-      });
-    }
-  };
+const AddChannel = ({mutate}) => {
+    const handleKeyUp = (evt) => {
+        if (evt.keyCode === 13) {
+            evt.persist();
+            mutate({
+                variables: {name: evt.target.value},
+                optimisticResponse: {
+                    addChannel: {
+                        name: evt.target.value,
+                        id: Math.round(Math.random() * -1000000),
+                        __typename: 'Channel',
+                    },
+                },
+                update: (store, {data: {addChannel}}) => {
+                    // Read the data from the cache for this query.
+                    const data = store.readQuery({query: channelsListQuery});
+                    // Add our channel from the mutation to the end.
+                    data.channels.push(addChannel);
+                    // Write the data back to the cache.
+                    store.writeQuery({query: channelsListQuery, data});
+                },
+            })
+                .then(res => {
+                    evt.target.value = '';
+                });
+        }
+    };
 
-  return (
-    <input
-      type="text"
-      placeholder="New channel"
-      onKeyUp={handleKeyUp}
-    />
-  );
+    return (
+        <input
+            type="text"
+            placeholder="New channel"
+            onKeyUp={handleKeyUp}
+        />
+    );
 };
 
 const addChannelMutation = gql`
-  mutation addChannel($name: String!) {
-    addChannel(name: $name) {
-      id
-      name
+    mutation addChannel($name: String!) {
+        addChannel(name: $name) {
+            id
+            name
+        }
     }
-  }
 `;
 
 
 const AddChannelWithMutation = graphql(
-  addChannelMutation,
+    addChannelMutation,
 )(AddChannel);
 
 export default AddChannelWithMutation;
