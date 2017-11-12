52800db6b4da29b7a9a9c896455ca3f207a9994f
cuidadoso <alexander_s@front.ru>
2017-11-12 14:30:48 +0300

    Add subscription to client

--- client/src/components/ChannelDetails.js
+++ client/src/components/ChannelDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MessageList from './MessageList';
 import ChannelPreview from './ChannelPreview';
 import NotFound from './NotFound';
@@ -8,25 +8,62 @@ import {
     graphql,
 } from 'react-apollo';
 
-const ChannelDetails = ({ data: {loading, error, channel }, match }) => {
-  if (loading) {
-    return <ChannelPreview channelId={match.params.channelId}/>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-  if(channel === null){
-    return <NotFound />
-  }
+class ChannelDetails extends Component {
+    componentWillMount() {
+        this.props.data.subscribeToMore({
+            document: messagesSubscription,
+            variables: {
+                channelId: this.props.match.params.channelId,
+            },
+            updateQuery: (prev, {subscriptionData}) => {
+                if (!subscriptionData.data) {
+                    return prev;
+                }
+                const newMessage = subscriptionData.data.messageAdded;
+                // don't double add the message
+                if (!prev.channel.messages.find((msg) => msg.id === newMessage.id)) {
+                    return Object.assign({}, prev, {
+                        channel: Object.assign({}, prev.channel, {
+                            messages: [...prev.channel.messages, newMessage],
+                        })
+                    });
+                } else {
+                    return prev;
+                }
+            }
+        });
+    }
 
-  return (
-    <div>
-      <div className="channelName">
-        {channel.name}
-      </div>
-      <MessageList messages={channel.messages}/>
-    </div>);
-}
+    render() {
+        const { data: {loading, error, channel }, match } = this.props;
+
+        if (loading) {
+            return <ChannelPreview channelId={match.params.channelId}/>;
+        }
+        if (error) {
+            return <p>{error.message}</p>;
+        }
+        if(channel === null){
+            return <NotFound />
+        }
+
+        return (
+            <div>
+                <div className="channelName">
+                    {channel.name}
+                </div>
+                <MessageList messages={channel.messages}/>
+            </div>);
+    }
+};
+const messagesSubscription = gql`
+    subscription messageAdded($channelId: ID!) {
+        messageAdded(channelId: $channelId) {
+            id
+            text
+        }
+    }
+`
 
 export const channelDetailsQuery = gql`
   query ChannelDetailsQuery($channelId : ID!) {
@@ -41,8 +78,10 @@ export const channelDetailsQuery = gql`
   }
 `;
 
-export default (graphql(channelDetailsQuery, {
-  options: (props) => ({
-    variables: { channelId: props.match.params.channelId },
-  }),
-})(ChannelDetails));
+const ChannelDetailsWithData = graphql(channelDetailsQuery, {
+    options: (props) => ({
+        variables: { channelId: props.match.params.channelId },
+    }),
+})(ChannelDetails);
+
+export default ChannelDetailsWithData;
